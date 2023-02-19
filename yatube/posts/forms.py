7e2dee1df40990be86f4f514092c378096c2c8c4
Бабаян Армен <stavros1983@yandex.ru>
2023-02-19 12:04:52 +0400

    after 2 review with form labels

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
+        labels = {
+            'text': 'Текст записи',
+            'group': 'Группа'
+        }
